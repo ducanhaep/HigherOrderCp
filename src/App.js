3801f3975c65d3c7178346a1787ff93bb90f3122
ducanhaep <tranducanh1012@gmail.com>
2019-06-16 19:46:50 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+const withCouter = Component =>
+  class Hoc extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        count: 0
+      };
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    update = type => {
+      if (type === "Inc") {
+        this.setState(prevState => ({ count: prevState.count + 1 }));
+      } else if (type === "Dec") {
+        this.setState(({ count }) => ({ count: count - 1 }));
+      }
+    };
+
+    render() {
+      return <Component {...this.state} {...this.props} update={this.update} />;
+    }
+  };
+
+const Couter = ({ count, update }) => (
+  <div>
+    <button onClick={() => update("Inc")}>Inc</button>
+    {count}
+    <button onClick={() => update("Dec")}>Dec</button>
+  </div>
+);
+
+const App = withCouter(Couter)
 
 export default App;
